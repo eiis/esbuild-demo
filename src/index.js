ce9318faf42b176f8c8668113dbee7394e028cc5
zhangdi <1258956799@qq.com>
2023-09-01 10:09:26 +0800

    feat:add ESLint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-export default function add(...nums) {
-  return nums.reduce((acc, curr) => acc + curr, 0);
-}
+// export default function add(...nums) {
+//   return nums.reduce((acc, curr) => acc + curr, 0)
+// }
 // module.exports = add;
+import * as espree from 'espree'
+
+function add(...nums) {
+  return nums.reduce((acc, curr) => acc + curr, 0)
+}
+
+const ast = espree.parse('let foo = "bar"', { ecmaVersion: 6 })
+console.log(JSON.stringify(ast, null, 2))
+
+export default add
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import add from './index.js'
 
 // const add = require('./index')
 
-const result = add(1,2,3)
+const result = add(1, 2, 3)
+
+// const a = 1
+
+// console.log(a)
 
 // function add(...nums) {
 //   return nums.reduce((acc, curr) => acc + curr, 0);
