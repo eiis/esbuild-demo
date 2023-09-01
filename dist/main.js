e1c3eaa05357862ff73d7d9108e813cc7dc15104
zhangdi <1258956799@qq.com>
2023-09-01 10:53:28 +0800

    chore:remove dist

--- dist/main.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-function add(...nums) {
-  return nums.reduce((acc, curr) => acc + curr, 0);
-}
-var src_default = add;
-
-// src/main.js
-var result = src_default(1, 2, 3);
-console.log(result);
