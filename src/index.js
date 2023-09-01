2994b577cbc3963ef9f30a8085ace93f3279ad41
zhangdi <1258956799@qq.com>
2023-09-01 10:48:59 +0800

    feat:init

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 //   return nums.reduce((acc, curr) => acc + curr, 0)
 // }
 // module.exports = add;
-import * as espree from 'espree'
+// import * as espree from 'espree'
 
 function add(...nums) {
   return nums.reduce((acc, curr) => acc + curr, 0)
 }
 
-const ast = espree.parse('let foo = "bar"', { ecmaVersion: 6 })
-console.log(JSON.stringify(ast, null, 2))
+// const ast = espree.parse('let foo = "bar"', { ecmaVersion: 6 })
+// console.log(JSON.stringify(ast, null, 2))
 
 export default add
